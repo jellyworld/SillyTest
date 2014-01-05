8f6a25b3bc008152263b310b4669fcf72a4a587f
jellyworld <jellyworld2001@gmail.com>
2014-01-04 21:11:55 -0800

    Added Silly controller and view

---
+++ SillyTest/SillyTest/Controllers/SillyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SillyTest.Controllers
+{
+    public class SillyController : Controller
+    {
+        //
+        // GET: /Silly/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
